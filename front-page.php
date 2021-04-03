1210a105d5c23204c1b78450ea27b9a3c91a873c
Nikita Glazkov <zitrnik@gmail.com>
2021-04-03 14:45:16 +0300

    DEV: add styles for front page

---
+++ front-page.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template Name: Homepage Template
+ *
+ * @package Underscoresme
+ * @since Underscoresme 1.0
+ */
+
+get_header(); ?>
+<h1>Hello!</h1>
+<?php
+echo "It's OK to be ..."
+?>
