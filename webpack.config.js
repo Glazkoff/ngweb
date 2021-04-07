916bce31188588f2ed3fc98a1d507e8ea5418194
Nikita Glazkov <zitrnik@gmail.com>
2021-04-07 10:25:33 +0300

    DEV: add base for image assests processing

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,10 @@ module.exports = {
           name: "[path][name].[ext]",
         },
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
     ],
   },
   plugins: [
