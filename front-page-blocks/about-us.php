01e791c40117677b7142fed6cde78daad0ee3298
Darya Belyaeva <d.belyaeva1@gmail.com>
2021-04-25 17:04:32 +0300

    stop animation

--- front-page-blocks/about-us.php
+++ front-page-blocks/about-us.php
@@ -1,6 +1,12 @@
 <section class="about-us">
-  <h2 class="heading-for-services">О нас</h2>
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo mollis magna volutpat sagittis. Vivamus et dolor a velit commodo posuere. Sed dignissim volutpat lectus, non fermentum velit varius vitae. Proin non sem in mi ullamcorper sagittis. Donec et gravida purus, ut volutpat eros. Suspendisse sagittis, arcu eget vulputate gravida, arcu nunc eleifend velit, eu suscipit orci ex ut ante. Sed eget pulvinar dui, et pretium nunc. Donec condimentum libero in justo euismod scelerisque. Cras ultrices sit amet elit quis tincidunt. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed posuere nec ex at efficitur. Aenean ultrices, elit id egestas pulvinar, diam dui accumsan enim, vitae varius ipsum nulla vel tellus.</p>
+  <h2 class="heading-for-about">О нас</h2>
+  <div class="wrap-for-about">
+    <div class="description-for-about">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo mollis magna volutpat sagittis. Vivamus et dolor a velit commodo posuere. Sed dignissim volutpat lectus, non fermentum velit varius vitae. Proin non sem in mi ullamcorper sagittis. Donec et gravida purus, ut volutpat eros. Suspendisse sagittis, arcu eget vulputate gravida, arcu nunc eleifend velit, eu suscipit orci ex ut ante. Sed eget pulvinar dui, et pretium nunc. Donec condimentum libero in justo euismod scelerisque. Cras ultrices sit amet elit quis tincidunt. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed posuere nec ex at efficitur. Aenean ultrices, elit id egestas pulvinar, diam dui accumsan enim, vitae varius ipsum nulla vel tellus.</p>
+    </div>
+    <div class="images-for-about">
+    </div> 
+  </div>
   <div class="wrap-for-running-stroke">
 
     <div class="infinite-line programming"></div>
