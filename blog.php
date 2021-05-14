0b50ba0b9a5b9b37e7be0f6adb9cd926e9c009b4
Elena Bychkova <lena.bychkova.200012@gmail.com>
2021-05-14 16:46:07 +0300

    DEV: filter for blog was made

--- blog.php
+++ blog.php
@@ -11,45 +11,45 @@ get_header(); ?>
 <h2>Статьи по веб-дизайну и разработке</h2>
 <nav class="extra-navigation">
 <ul class="menu">
-<li><a href="#">Все</a></li>
-<li><a href="#">Дизайн</a></li>
-<li><a href="#">Разработка</a></li>
-<li><a href="#">Продвижение</a></li>
-</ul>		
+<li><a href="#" onclick="showEvery()">Все</a></li>
+<li><a href="#" class="design" onclick="showDesign()">Дизайн</a></li>
+<li><a href="#" class="dev" onclick="showDev()">Разработка</a></li>
+<li><a href="#" class="prom"onclick="showProm()">Продвижение</a></li>
+</ul>
 </nav>
-<div class="articles">
-  <div class="one-article">
+<div id="articles">
+  <div class="article dev">
     <p class="tag">Разработка</p>
 <img src="http://sun9-12.userapi.com/c11351/u135645498/137910343/x_b3966547.jpg" alt="картинка статьи">
 <h6>SEO продвижение сайта в Яндекс:</h6>
 <small>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Potenti eget erat ultricies in habitant risus etiam egestas et. Ac, neque, iaculis nec vulputate tempus hendrerit. Lacus, ipsum elit nibh fusce in vel. Et faucibus mauris, accumsan, elit.</small>
 </div>
-<div class="one-article">
+<div class="article design">
+  <p class="tag">Дизайн</p>
+<img src="http://sun9-12.userapi.com/c11351/u135645498/137910343/x_b3966547.jpg" alt="картинка статьи">
+<h6>SEO продвижение сайта в Яндекс:</h6>
+<small>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Potenti eget erat ultricies in habitant risus etiam egestas et. Ac, neque, iaculis nec vulputate tempus hendrerit. Lacus, ipsum elit nibh fusce in vel. Et faucibus mauris, accumsan, elit.</small>
+</div>
+<div class="article prom">
+  <p class="tag">Продвижение</p>
+<img src="http://sun9-12.userapi.com/c11351/u135645498/137910343/x_b3966547.jpg" alt="картинка статьи">
+<h6>SEO продвижение сайта в Яндекс:</h6>
+<small>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Potenti eget erat ultricies in habitant risus etiam egestas et. Ac, neque, iaculis nec vulputate tempus hendrerit. Lacus, ipsum elit nibh fusce in vel. Et faucibus mauris, accumsan, elit.</small>
+</div>
+<div class="article dev">
   <p class="tag">Разработка</p>
 <img src="http://sun9-12.userapi.com/c11351/u135645498/137910343/x_b3966547.jpg" alt="картинка статьи">
 <h6>SEO продвижение сайта в Яндекс:</h6>
 <small>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Potenti eget erat ultricies in habitant risus etiam egestas et. Ac, neque, iaculis nec vulputate tempus hendrerit. Lacus, ipsum elit nibh fusce in vel. Et faucibus mauris, accumsan, elit.</small>
 </div>
-<div class="one-article">
-  <p class="tag">Разработка</p>
-<img src="http://sun9-12.userapi.com/c11351/u135645498/137910343/x_b3966547.jpg" alt="картинка статьи">
-<h6>SEO продвижение сайта в Яндекс:</h6>
-<small>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Potenti eget erat ultricies in habitant risus etiam egestas et. Ac, neque, iaculis nec vulputate tempus hendrerit. Lacus, ipsum elit nibh fusce in vel. Et faucibus mauris, accumsan, elit.</small>
-</div>
-<div class="one-article">
-  <p class="tag">Разработка</p>
-<img src="http://sun9-12.userapi.com/c11351/u135645498/137910343/x_b3966547.jpg" alt="картинка статьи">
-<h6>SEO продвижение сайта в Яндекс:</h6>
-<small>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Potenti eget erat ultricies in habitant risus etiam egestas et. Ac, neque, iaculis nec vulputate tempus hendrerit. Lacus, ipsum elit nibh fusce in vel. Et faucibus mauris, accumsan, elit.</small>
-</div>
-<div class="one-article">
+<div class="article dev">
 <p class="tag">Разработка</p>
 <img src="http://sun9-12.userapi.com/c11351/u135645498/137910343/x_b3966547.jpg" alt="картинка статьи">
 <h6>SEO продвижение сайта в Яндекс:</h6>
 <small>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Potenti eget erat ultricies in habitant risus etiam egestas et. Ac, neque, iaculis nec vulputate tempus hendrerit. Lacus, ipsum elit nibh fusce in vel. Et faucibus mauris, accumsan, elit.</small>
 </div>
-<div class="one-article">
-  <p class="tag">Разработка</p>
+<div class="article prom">
+  <p class="tag">Продвижение</p>
 <img src="http://sun9-12.userapi.com/c11351/u135645498/137910343/x_b3966547.jpg" alt="картинка статьи">
 <h6>SEO продвижение сайта в Яндекс:</h6>
 <small>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Potenti eget erat ultricies in habitant risus etiam egestas et. Ac, neque, iaculis nec vulputate tempus hendrerit. Lacus, ipsum elit nibh fusce in vel. Et faucibus mauris, accumsan, elit.</small>
@@ -57,5 +57,46 @@ get_header(); ?>
 </div>
 </div>
 </section>
-<?php get_footer();
-?>
+<?php get_footer(); ?>
+<script>
+function showEvery(){
+var items_el = document.getElementById('articles');
+  var items = items_el.getElementsByClassName('article');
+  for (var i=0; i<items.length; i++) {
+    	items[i].style.display = 'block';
+};
+};
+function showDesign(){
+var items_el = document.getElementById('articles');
+  var items = items_el.getElementsByClassName('article');
+  for (var i=0; i<items.length; i++) {
+  	if (items[i].classList.contains("design")) {
+    	items[i].style.display = 'block';
+    } else {
+    	items[i].style.display = 'none';
+    }
+  }
+};
+function showDev(){
+var items_el = document.getElementById('articles');
+  var items = items_el.getElementsByClassName('article');
+  for (var i=0; i<items.length; i++) {
+  	if (items[i].classList.contains("dev")) {
+    	items[i].style.display = 'block';
+    } else {
+    	items[i].style.display = 'none';
+    }
+  }
+};
+function showProm(){
+var items_el = document.getElementById('articles');
+  var items = items_el.getElementsByClassName('article');
+  for (var i=0; i<items.length; i++) {
+  	if (items[i].classList.contains("prom")) {
+    	items[i].style.display = 'block';
+    } else {
+    	items[i].style.display = 'none';
+    }
+  }
+};
+</script>
