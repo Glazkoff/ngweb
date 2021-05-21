e1d3a1e7e19645742501834aae7e451e596d9548
nvkolezneva <nvkolezneva@gmail.com>
2021-05-21 11:43:44 +0300

    fix: sleek header-transition

--- src/headroom-init.js
+++ src/headroom-init.js
@@ -4,7 +4,15 @@ export default function () {
     let myElement = document.querySelector("header");
     console.log('header', myElement);
 
-    let headroom = new Headroom(myElement);
+    let headroom = new Headroom(myElement, {
+  "offset": 30,
+  "tolerance": 12,
+  "classes": {
+    "initial": "animated",
+    "pinned": "slideDown",
+    "unpinned": "slideUp"
+  }
+});
     console.log('headroom', headroom);
 
     headroom.init();
