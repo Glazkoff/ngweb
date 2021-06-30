39c5120fc9eaac0a39c99b43298fa425812973ca
Nikita Glazkov <zitrnik@gmail.com>
2021-06-30 08:41:52 +0300

    dev: add google analytics and yandex metrica tags into funxtions

--- functions.php
+++ functions.php
@@ -165,6 +165,46 @@ function ngwebstudio_scripts()
 }
 add_action("wp_enqueue_scripts", "ngwebstudio_scripts");
 
+add_action("wp_head", "true_metrika");
+
+function true_metrika()
+{
+	?>
+	<!-- Yandex.Metrika counter -->
+	<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(82291090, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/82291090" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+	<?php
+}
+
+add_action("wp_head", "true_analytica");
+
+function wp_analyticass()
+{
+	?>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-QXF57ZWWFN"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-QXF57ZWWFN');
+</script>
+<?php
+}
+
 /**
  * Implement the Custom Header feature.
  */
