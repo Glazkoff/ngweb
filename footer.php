7569776bb168f6dce4182cdc02ae92e448dec9b0
Nikita Glazkov <zitrnik@gmail.com>
2021-07-01 20:32:06 +0300

    DEV: add mailto and tel links

--- footer.php
+++ footer.php
@@ -9,43 +9,48 @@
  *
  * @package ngwebstudio
  */
-
 ?>
 	<footer id="colophon" class="site-footer">
 		<div class="footer-custom"><div class="test-centert">
 			<ul>
-				<li><a href="https://nglazkov.ru/#" aria-current="page">Главная</a></li>
-				<li><a href="https://nglazkov.ru/#about-us" aria-current="page">О нас</a></li>
-				<li><a href="https://nglazkov.ru/#services" aria-current="page">Услуги</a></li>
-				<li><a href="https://nglazkov.ru/#contact-form" aria-current="page">Обратная связь</a></li>
+				<li><a href="#" aria-current="page">Главная</a></li>
+				<li><a href="#about-us" aria-current="page">О нас</a></li>
+				<li><a href="#services" aria-current="page">Услуги</a></li>
+				<li><a href="#contact-form" aria-current="page">Обратная связь</a></li>
 			</ul></div>
 			<div><ul>
-				<li><a href="<?php echo esc_url( __( 'https://www.facebook.com/nglazkovstudio/', 'ngwebstudio' ) ); ?> "target="_blank">
+				<li><a href="<?php echo esc_url(
+    	__("https://www.facebook.com/nglazkovstudio/", "ngwebstudio")
+    ); ?> "target="_blank">
 				Facebook</a></li>
-				<li><a href="<?php echo esc_url( __( 'https://www.instagram.com/nglazkov_studio/', 'ngwebstudio' ) ); ?> "target="_blank">
+				<li><a href="<?php echo esc_url(
+    	__("https://www.instagram.com/nglazkov_studio/", "ngwebstudio")
+    ); ?> "target="_blank">
 				Instagram</a></li>
-				<li><a href="<?php echo esc_url( __( 'https://vk.com/nglazkov_studio', 'ngwebstudio' ) ); ?> "target="_blank">
+				<li><a href="<?php echo esc_url(
+    	__("https://vk.com/nglazkov_studio", "ngwebstudio")
+    ); ?> "target="_blank">
 				ВКонтакте</a></li>
 			</ul></div>
 				<div class="footer-contact"><ul>
-				<li>+7(967)335-79-67</li>
-				<li>contact@nglazkov.ru</li>
+				<li><a href="tel:+79673357967">+7 (967) 335-79-67</a></li>
+				<li><a href="mailto:contact@nglazkov.ru?subject=Заявка на заказ">contact@nglazkov.ru</a></li>
 			</ul></div></div>
 			<div>
 				<hr>
-				<h6>NIKITA GLAZKOV <?php echo date('Y'); ?></h6>
+				<h6>NIKITA GLAZKOV <?php echo date("Y"); ?></h6>
 			</div>
-			<!-- <a href="<?php echo esc_url( __( 'https://nglazkov.ru', 'ngwebstudio' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'ngwebstudio' ), 'WordPress' );
-				?>
+			<!-- <a href="<?php echo esc_url(__("https://nglazkov.ru", "ngwebstudio")); ?>">
+				 /* translators: %s: CMS name, i.e. WordPress. */<?php
+/* translators: %s: CMS name, i.e. WordPress. */
+?>printf(esc_html__("Proudly powered by %s", "ngwebstudio"), "WordPress"); ?>
 			</a> -->
 			<!-- <span class="sep"> | </span> -->
 				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				// printf( esc_html__( 'Theme: %1$s by %2$s.', 'ngwebstudio' ), 'ngwebstudio', '<a href="http://nglazkov.ru">Nikita Glazkov</a>' );
-				// ?>
+/* translators: 1: Theme name, 2: Theme author. */
+// printf( esc_html__( 'Theme: %1$s by %2$s.', 'ngwebstudio' ), 'ngwebstudio', '<a href="http://nglazkov.ru">Nikita Glazkov</a>' );
+//
+?>
 		<!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
