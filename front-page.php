6aa5027e9862de195d6a570889be17df7439a18f
Darya Belyaeva <d.belyaeva1@gmail.com>
2021-07-07 01:42:07 +0400

    dev:form  mobile version

--- front-page.php
+++ front-page.php
@@ -16,32 +16,4 @@ include "front-page-blocks/contact-form.php";
 get_footer();
 ?>
 
-<!-- <h1>H1 - Заголовок 1 </h1>
-    <h2>H2 - Заголовок 2 </h2>
-    <h3>H3 - Заголовок 3</h3>
-    <h4>H4 - Заголовок 4</h4>
-    <h5>H5 - Заголовок 5</h5>
-    <h6>H6 - Заголовок 6</h6> 
-    <a href="yandex.ru">Link!</a> 
-    <hr>
-    <p>
-      p -   
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem,
-      magnam nisi, suscipit voluptatem obcaecati quas, reprehenderit tempora aut
-      eveniet expedita necessitatibus. Dolor commodi mollitia corporis natus
-      expedita a qui hic!
-    </p>
-    
-    <small>
-      small - 
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae
-      recusandae voluptates ab ad soluta minus excepturi! Aliquid debitis
-      eligendi minima ipsam voluptatum inventore eveniet in, voluptate quibusdam
-      dignissimos sed magni!
-    </small>
-    <button>Кнопка</button>
-    <label for="input">It's label for input</label>
-    <input type="text" id="input">
-    <label for="input2" class="red">It's label for input</label>
-    <input type="text" id="input2" class="red">
-<hr> -->
+ ?>
