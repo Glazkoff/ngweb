a13fdc720e9493d24183587ec7d0fa0a5a7c10f5
Nikita Glazkov <zitrnik@gmail.com>
2021-07-07 15:51:35 +0300

    dev: cosmetic changes

--- front-page-blocks/main-block.php
+++ front-page-blocks/main-block.php
@@ -1,6 +1,6 @@
 <section class="main-block">
   <div class="dynamic-heading__block">
-    <h2 id="main-heading" class="dynamic-heading">Разрабатываем<br>сайты, которые<br> притягивают<br>внимание</h2>  
+    <h2 id="main-heading" class="dynamic-heading main-top-block">Разрабатываем<br>сайты, которые<br> притягивают<br>внимание</h2>  
   </div>
   <div id="main-sugar">
     <svg id="main-sugar__svg">
